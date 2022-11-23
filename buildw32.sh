a29296e497ec208632367184a3c89b4893ca8f4e
logy xpoy <xpoy@live.cn>
2022-11-23 15:17:09 +0800

    test Build

--- buildw32.sh
+++ buildw32.sh
@@ -1,16 +1,13 @@
-sudo update-alternatives --auto --config x86_64-w64-mingw32-gcc
-sudo update-alternatives --auto  --config x86_64-w64-mingw32-g++
-sudo update-alternatives --auto --config x86_64-w64-mingw32-gfortran
-sudo update-alternatives --auto --config x86_64-w64-mingw32-gnat
+sudo update-alternatives--config x86_64-w64-mingw32-gcc
+sudo update-alternatives --config x86_64-w64-mingw32-g++
+sudo update-alternatives--config x86_64-w64-mingw32-gfortran
+sudo update-alternatives--config x86_64-w64-mingw32-gnat
 
-sudo update-alternatives --auto --config i686-w64-mingw32-gcc
-sudo update-alternatives --auto --config i686-w64-mingw32-g++
-sudo update-alternatives --auto --config i686-w64-mingw32-gfortran
-sudo update-alternatives --auto --config i686-w64-mingw32-gnat
+sudo update-alternatives--config i686-w64-mingw32-gcc
+sudo update-alternatives--config i686-w64-mingw32-g++
+sudo update-alternatives--config i686-w64-mingw32-gfortran
+sudo update-alternatives--config i686-w64-mingw32-gnat
 
-
-#进入工作目录
-cd /Volumes/aapt
 #使用cmake生成linux构建文件，并最小化编译产物
 cmake -H"./" -B"./build-cmake-linux" -DCMAKE_BUILD_TYPE=MinSizeRel
 #使用linux环境生成aapt2所需的头文件
